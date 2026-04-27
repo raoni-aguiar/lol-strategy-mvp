52a9f073d65a6451f42256187b2c961ea7fc2992
raoni-aguiar <inoarcam@gmail.com>
2026-04-26 21:48:11 -0300

    cleanup: remove unused files

--- serve.cjs
+++
@@ -1,4 +0,0 @@
-const { execSync } = require('child_process')
-const port = process.env.PORT || 3000
-console.log(`Serving on port ${port}`)
-execSync(`npx serve dist -l ${port}`, { stdio: 'inherit' })
--- start-frontend.js
+++
@@ -1,9 +0,0 @@
-import { createServer } from 'vite'
-import { exec } from 'child_process'
-
-const port = process.env.PORT || 4173
-exec(`npx vite preview --host --port ${port}`, (err, stdout, stderr) => {
-  if (err) console.error(err)
-  console.log(stdout)
-  console.error(stderr)
-})
